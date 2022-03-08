cb78f85f2e65b7c310502c0dd14495fcee96c611
fum <66768334+funbeedev@users.noreply.github.com>
2022-03-08 20:00:55 +0000

    setup c2d messaging -set article id as config variable -handle when comment is deleted -only publish if top level comment changed

--- sender_app_hack.py
+++ sender_app_hack.py
@@ -6,6 +6,7 @@ import paho.mqtt.client as mqtt # First run: pip install paho-mqtt
 from datetime import datetime
 import time
 import json
+from azure.iot.hub import IoTHubRegistryManager
 
 # TODO: dont use globals
 
@@ -16,7 +17,10 @@ config = ConfigParser()
 config.read(config_file)
 
 # get all settings from config file
+article_id = config.get('config_cloud', 'article_id')
 conn_type = config.get('config_cloud', 'conn_type')
+azure_conn_string = config.get('config_cloud', 'azure_conn_string')
+device_id = config.get('config_cloud', 'device_id')
 mqtt_broker = config.get('config_cloud', 'mqtt_broker')
 sub_topic_1 = config.get('config_cloud', 'sub_topic_1')
 sub_topic_2 = config.get('config_cloud', 'sub_topic_2')
@@ -26,11 +30,15 @@ print(f'conn_type: {conn_type} | broker: {mqtt_broker}  | sub_topic_1: {sub_topi
 
 
 def dev_api_request():
-    
+    article_url = 'https://dev.to/api/comments?a_id=' + article_id
+    response = requests.get(article_url)
 
-    # use dummy json from file for testing
-    with open('json.json') as f:
-        response_json = json.load(f)
+    # convert response to json format
+    response_json = response.json()
+
+    # # use dummy json from file for testing
+    # with open('json.json') as f:
+    #     response_json = json.load(f)
 
     print(f"number of fields in json / comment blocks: {len(response_json)}")
     print(f"number of keys in each dict: {len(response_json[0])}")
@@ -42,10 +50,13 @@ def dev_api_request():
 
     # loop over each comment block represented as json (a list of dicts)
     for x in range(len(response_json)):
+
         # extract username from highest level comment block
-        print(response_json[x]['user']['username'])
-        commenters_usernames.append(response_json[x]['user']['username'])
-        commenters_comments.append(response_json[x]['body_html'])
+        # first check that user field has data - if comment is showing as deleted this will be empty
+        if ((response_json[x]['user']) != {}):
+            print(response_json[x]['user']['username'])
+            commenters_usernames.append(response_json[x]['user']['username'])
+            commenters_comments.append(response_json[x]['body_html'])
 
         # start scan for sub comment blocks - represented by 'children' key
         dict_data = response_json[x]
@@ -68,7 +79,6 @@ def dev_api_request():
     return(commenters_usernames, commenters_comments)
 
 
-
 def html_to_text(commenters_usernames, commenters_comments):
 
     # to hold text only version of comments
@@ -81,22 +91,21 @@ def html_to_text(commenters_usernames, commenters_comments):
         # print(raw_html.get_text())
         commenters_comments_text.append(raw_html.get_text())
 
-    # print each comment
     # print(*commenters_comments_text, sep='-------\n')
-    for x, y in enumerate(commenters_comments_text):#
-        print(x+1)
-        print(y)
-        
+    # for x, y in enumerate(commenters_comments_text):
+    #     print(x+1)
+    #     print(y)
+  
     return commenters_comments_text
 
 
 def text_to_speech(text):
-    
+
     # read text out loud
     engine = pyttsx3.init()
     engine.say("Start sender app!")
     engine.runAndWait()
-    
+
     # engine.say(text)
     # engine.runAndWait()
 
@@ -123,7 +132,7 @@ def on_message(client, data, msg):
     print("topic: %s" % topic_received)
     print("data: %s" % data_received)
 
-    if (msg.topic == ''): 
+    if (msg.topic == ''):
         None
 
 
@@ -146,43 +155,58 @@ def mqtt_setup(broker, topic_1, topic_2):
     return client
 
 
-def send_iothub_to_device():
-    print("Sending directly from IoTHub to Device")
+def send_iothub_to_device(data):
+    # Create IoTHubRegistryManager
+    registry_manager = IoTHubRegistryManager(azure_conn_string)
+
+    print(f"\nSending Cloud -> Device:\n {data}")
+
+    # send message to device
+    props={}
+    registry_manager.send_c2d_message(device_id, data, properties=props)
+
+    input("Press Enter to continue...\n")
 
 
-def run_mqtt_messaging(commenters_usernames, commenters_comments_text):
+def run_messaging(commenters_usernames, commenters_comments_text):
     # setup mqtt conn to broker and topics
     client = mqtt_setup(mqtt_broker, sub_topic_1, sub_topic_2)
 
     # commenters_comments_text = '123456789'  # dummy
+    last_msg_publish = 'temp'
+    msg_publish = ''
 
     while True:
         time.sleep(3)
 
-        # -> send just one piece of data at a time
-        x = len(commenters_comments_text) - 1 # last comment in post list
-        x = 0  # first comment
-
         # build up msg to publish
-        msg_publish = " username: " + commenters_usernames[x] + " ," + " comment: " + commenters_comments_text[x]
-        print(f"Publish latest comment: {msg_publish}")
+        msg_publish = " username: " + commenters_usernames[0] + " ," + " comment: " + commenters_comments_text[0]
 
-        # publish to topic
-        client.publish(pub_topic_1, msg_publish)
+        # publish message to device only if there's new data
+        if(msg_publish != last_msg_publish):
+            last_msg_publish = msg_publish
 
-        # -> sending a burst of data at once
-        # for x in range(len(commenters_usernames)):
-        #     # build up msg to publish
-        #     msg_publish = str(x+1) + " username: " + commenters_usernames[x] + " comment: " + commenters_comments_text[x]
-        #     # publish to topic
-        #     client.publish(publish_topic, msg_publish)
+            print(f"Publish latest comment: {msg_publish}")
 
-        # also send message via sdk if stated
-        if(conn_type == 'c2d'):
-            send_iothub_to_device()
+            # publish to topic
+            client.publish(pub_topic_1, msg_publish)
+
+            # -> sending a burst of data at once
+            # for x in range(len(commenters_usernames)):
+            #     # build up msg to publish
+            #     msg_publish = str(x+1) + " username: " + commenters_usernames[x] + " comment: " + commenters_comments_text[x]
+            #     # publish to topic
+            #     client.publish(publish_topic, msg_publish)
+
+            # also send message directly to cloud if configured
+            if(conn_type == 'cloud2device'):
+                send_iothub_to_device(msg_publish)
+                None
+        else:
+            print("No change to Comments")
 
         # refresh dev api call to get latest comments
-        print("--- REFRESH API ---")
+        print("\n--- REFRESH API CALL ---")
         commenters_usernames, commenters_comments = dev_api_request()
         commenters_comments_text = html_to_text(commenters_usernames, commenters_comments)
 
@@ -192,7 +216,7 @@ def start_program_flow():
     commenters_usernames, commenters_comments = dev_api_request()
     commenters_comments_text = html_to_text(commenters_usernames, commenters_comments)
     text_to_speech(commenters_comments_text)
-    run_mqtt_messaging(commenters_usernames, commenters_comments_text)
+    run_messaging(commenters_usernames, commenters_comments_text)
 
 
 if __name__ == "__main__":
